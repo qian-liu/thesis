20481adc733fcdc205c6b85f76606a98e48f6a99
qian-liu <qianliu.bjut@gmail.com>
2018-01-14 17:10:38 +0000

    unifie texshop and texstudio

---
+++ diff_sh.sh
@@ -0,0 +1,9 @@
+latexdiff Chapter1_1.tex Chapter1.tex > diff1.tex
+latexdiff Chapter2_1.tex Chapter2.tex > diff2.tex
+latexdiff Chapter3_1.tex Chapter3.tex > diff3.tex
+latexdiff Chapter4_1.tex Chapter4.tex > diff4.tex
+latexdiff Chapter5_1.tex Chapter5.tex > diff5.tex
+#latexdiff Chapter6_1.tex Chapter6.tex > diff6.tex
+latexdiff Conclusion_1.tex Conclusion.tex > diff7.tex
+latexdiff abstract_1.tex abstract.tex > diff0.tex
+
